e951674dd74984d4d152e1e72bea00c763e877d3
Richard Spence <richardspence@Richards-MacBook-Air.local>
2017-09-29 16:05:57 -0700

    First Commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function User(firstName, lastName, age, gender){
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.age = age;
+  this.gender = gender;
+};
+
+var user1 = new User('John', 'Smith', 26, 'male');
+var user200 = new User('Jill', 'Robinson', 25,'female');
